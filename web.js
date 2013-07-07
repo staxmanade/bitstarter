b680c0b02813f896a1944b87c721c6cb69127d94
Jason Jarrett <jason@elegantcode.com>
2013-07-07 05:21:43 +0000

    Write out contents of the index.html file

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile("./index.html", function(err, data) {
+    if(err) {
+      response.send(err);
+      return;
+    }
+
+    var buf = new Buffer(data);
+    
+    response.send(buf.toString());
+  });
 });
 
 var port = process.env.PORT || 5000;
